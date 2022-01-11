63257214514109f7c33e48e2c46aabf694d3f188
ILikeMonday <worldrocker64@gmail.com>
2022-01-11 22:17:35 +0700

    all done

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void insertionSort(T d[],int N)
+{ 
+    int i, j;
+	int key; 
+    for (i =0; i < N-1; i++)
+    { 
+        key = d[i+1]; 
+        j = i; 
 
-}
+        while (j >= 0 && d[j] < key )
+        { 
+            d[j + 1] = d[j]; 
+            j = j - 1; 
+        } 
+        d[j + 1] = key; 
+		
+			cout << "Pass " << i+1 << ":";
+			for(int a=0;a<N;a++){
+				cout << d[a] << " ";
+			}
+			cout << "\n";
+    } 
+	
+} 
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +39,5 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+
